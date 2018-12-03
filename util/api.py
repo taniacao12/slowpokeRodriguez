76af33021d91878cc9d86a269660740620791af4
Johnson Li <jli39@stuy.edu>
2018-12-03 01:17:19 -0500

    got api working and recipes show up but dictionaries in jinja is weird

--- util/api.py
+++ util/api.py
@@ -3,10 +3,23 @@ import json
 from urllib.request import Request, urlopen
 
 def search():
-    cApi=urlopen("https://www.food2fork.com/api/search?key=116b5832d48d9f642361a4e878b5cf8f&q=" + "chicken")
-    cApi = cApi.read()
-    dict = json.loads(cApi)
+    URL = "https://www.food2fork.com/api/search?key=116b5832d48d9f642361a4e878b5cf8f&q=" + "chicken"
 
-    print(dict)
+    response = urlopen(Request(URL, headers={'User-Agent': 'Mozilla/5.0'})).read()
+    info = json.loads(response)
+    recipes_res = info["recipes"]
+
+    recipes = {}
+    for num in range(5):
+        title = recipes_res[num]["title"]
+
+        recipes[title] = [
+            recipes_res[num]["publisher"],
+            recipes_res[num]["source_url"],
+            recipes_res[num]["image_url"]
+        ]
+
+    print(recipes);
+    return recipes;
 
 search()
