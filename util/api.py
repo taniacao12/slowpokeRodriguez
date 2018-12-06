a3c0d75027e1a005bc4307da2cf1cf4efe93b6d3
mhowlader <mhowlader@stuy.edu>
2018-12-06 04:37:18 -0500

    added api keys

--- util/api.py
+++ util/api.py
@@ -2,9 +2,25 @@ import json
 #import urllib.request
 from urllib.request import Request, urlopen
 
-def search():
-    URL = "https://www.food2fork.com/api/search?key=116b5832d48d9f642361a4e878b5cf8f&q=" + "chicken"
+api_list = [] # list o api keys
+curInd = 0 # current index of working api_keys
 
+
+
+with open("util/api_keys.txt") as file:
+    api_keys = file.read()
+    api_list = api_keys.split(",")
+
+
+def search():
+
+    keyMain = ""
+    for key in api_list:
+        if verify_key(key) == True:
+            keyMain = key
+        else:
+            continue
+    URL = "https://www.food2fork.com/api/search?key=" + keyMain
     response = urlopen(Request(URL, headers={'User-Agent': 'Mozilla/5.0'})).read()
     info = json.loads(response)
     recipes_res = info["recipes"]
@@ -18,8 +34,18 @@ def search():
             recipes_res[num]["source_url"],
             recipes_res[num]["image_url"]
         ]
+    print("it working" + key)
+    return recipes
 
-    print(recipes);
-    return recipes;
+
+
+def verify_key(key):
+
+    try:
+        URL = "https://www.food2fork.com/api/search?key=" + key
+        response = urlopen(Request(URL, headers={'User-Agent': 'Mozilla/5.0'})).read()
+        return True
+    except:
+        return False
 
 search()
